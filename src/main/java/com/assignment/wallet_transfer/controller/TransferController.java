76212c9a5f4e7fcab1ecc76269b94ca1bd9646d3
himanshukumar <yaduvanshier05@gmail.com>
2026-06-16 08:58:19 +0530

    Commit Swagger Changes

--- src/main/java/com/assignment/wallet_transfer/controller/TransferController.java
+++ src/main/java/com/assignment/wallet_transfer/controller/TransferController.java
@@ -3,6 +3,8 @@ package com.assignment.wallet_transfer.controller;
 import com.assignment.wallet_transfer.dtos.TransferRequest;
 import com.assignment.wallet_transfer.dtos.TransferResponse;
 import com.assignment.wallet_transfer.service.TransferService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transfers")
+@Tag(name = "Wallet APIs", description = "Wallet transfer operations")
 @RequiredArgsConstructor
 public class TransferController {
 
     private final TransferService transferService;
 
     @PostMapping
+    @Operation(summary = "Transfer money between wallets")
     public ResponseEntity<TransferResponse> transfer(
             @RequestBody @Valid TransferRequest request,
             @RequestHeader("Idempotency-Key")
